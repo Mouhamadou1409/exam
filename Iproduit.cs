4ff0f0825800998c5c317952a2006d66f2f594ab
Mouhamadou1409 <130991163+Mouhamadou1409@users.noreply.github.com>
2023-06-14 13:27:07 +0000

    Add files via upload

---
+++ Iproduit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPhar1
+{
+    internal interface Iproduit
+    {
+    }
+}
